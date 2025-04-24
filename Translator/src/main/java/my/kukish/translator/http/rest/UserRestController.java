3f99bdd8532aa58769b516d24bfa960061336e46
churkaa <semenovegor546@gmail.com>
2025-04-24 22:54:15 +0300

    CRUD Operation for UserRestController

--- Translator/src/main/java/my/kukish/translator/http/rest/UserRestController.java
+++ Translator/src/main/java/my/kukish/translator/http/rest/UserRestController.java
@@ -2,11 +2,14 @@ package my.kukish.translator.http.rest;
 
 import lombok.RequiredArgsConstructor;
 import my.kukish.translator.database.service.UserService;
+import my.kukish.translator.dto.UserCreateEditDto;
 import my.kukish.translator.dto.UserReadDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,7 +22,35 @@ public class UserRestController {
 
     @GetMapping
     @ResponseBody
-    public List<UserReadDto> findAll(){
+    public List<UserReadDto> findAll() {
         return userService.findAll();
+    }
+
+    @GetMapping("/id")
+    @ResponseBody
+    public UserReadDto findById(@PathVariable("id") Long id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserReadDto crate(@Validated @RequestBody UserCreateEditDto user) {
+        userService.create(user);
+        return userService.create(user);
+    }
+
+    @PutMapping("/id")
+    public UserReadDto update(@PathVariable("id") Long id, @Validated @RequestBody UserCreateEditDto user) {
+        return userService.update(id, user)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+    }
+
+    @DeleteMapping("/id")
+    public void delete(@PathVariable("id") Long id) {
+        if(!userService.delete(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
     }
 }
