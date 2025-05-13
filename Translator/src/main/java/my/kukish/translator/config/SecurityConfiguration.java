a123cab63c10c7e1bccb7292db89216908200a4c
churkaa <semenovegor546@gmail.com>
2025-05-14 00:00:30 +0300

    Spring Configure

---
+++ Translator/src/main/java/my/kukish/translator/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package my.kukish.translator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests
+                                .anyRequest().authenticated())
+                //.httpBasic(Customizer.withDefaults())
+                .formLogin(login ->
+                        login.loginPage("/login").defaultSuccessUrl("/"))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JSESSIONID"));
+        return http.build();
+    }
+}
